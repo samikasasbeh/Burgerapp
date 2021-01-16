30eaf3e6a24a7fe17171d5c97e045b3a71b5860a
Sami <sami.kasasbehdev@gmail.com>
2021-01-16 22:28:07 +0200

    Fixing validation

--- src/Checkout/ContactData/ContactData.js
+++ src/Checkout/ContactData/ContactData.js
@@ -115,17 +115,17 @@ class ContactData extends Component{
         
     }
     checkValidity(value, rules){
-        let isValid =false;
+        let isValid =true;
 
 
         if(rules.required){
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         }
         if (rules.minLength){
-            isValid = value.length >= rules.minlength
+            isValid = value.length >= rules.minlength && isValid
         }
         if (rules.maxLength){
-            isValid = value.length <= rules.maxLength
+            isValid = value.length <= rules.maxLength && isValid
         }
         return isValid;
     }
